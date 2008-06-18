b0653c2ba3ef69d2e93316c778db5c5349c0ed48
devon <devon@devon-2.local>
2008-06-18 09:54:50 +0800

    bug fix for editor: pop up window size in ie

--- assets/public/javascripts/tiny_mce/plugins/curblyadvimage/editor_plugin_src.js
+++ assets/public/javascripts/tiny_mce/plugins/curblyadvimage/editor_plugin_src.js
@@ -19,7 +19,7 @@
 				ed.windowManager.open({
 					file : url + '/image.htm',
 					width : 480 + parseInt(ed.getLang('curblyadvimage.delta_width', 0)),
-					height : 385 + parseInt(ed.getLang('curblyadvimage.delta_height', 0)),
+					height : 415 + parseInt(ed.getLang('curblyadvimage.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url
